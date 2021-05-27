65c9c9f8e7df4627e296e3cd1e20c7cb5c95d2ba
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-27 14:46:48 -0400

    add properties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,5 +1,3 @@
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
 const { Pool } = require("pg");
 
 const pool = new Pool({
@@ -11,11 +9,6 @@ const pool = new Pool({
 
 /// Users
 
-/**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
 const getUserWithEmail = function (email) {
   const queryString = `
   SELECT * FROM users
@@ -28,11 +21,6 @@ const getUserWithEmail = function (email) {
 };
 exports.getUserWithEmail = getUserWithEmail;
 
-/**
- * Get a single user from the database given their id.
- * @param {string} id The id of the user.
- * @return {Promise<{}>} A promise to the user.
- */
 const getUserWithId = function (id) {
   const queryString = `
   SELECT * FROM users
@@ -45,11 +33,6 @@ const getUserWithId = function (id) {
 };
 exports.getUserWithId = getUserWithId;
 
-/**
- * Add a new user to the database.
- * @param {{name: string, password: string, email: string}} user
- * @return {Promise<{}>} A promise to the user.
- */
 const addUser = function (user) {
   const queryString = `
   INSERT INTO users (name, email, password)
@@ -64,11 +47,6 @@ exports.addUser = addUser;
 
 /// Reservations
 
-/**
- * Get all reservations for a single user.
- * @param {string} guest_id The id of the user.
- * @return {Promise<[{}]>} A promise to the reservations.
- */
 const getAllReservations = function (guest_id, limit = 10) {
   const queryString = `
     SELECT properties.*, reservations.*, avg(rating) as average_rating
@@ -91,12 +69,6 @@ exports.getAllReservations = getAllReservations;
 
 /// Properties
 
-/**
- * Get all properties.
- * @param {{}} options An object containing query options.
- * @param {*} limit The number of results to return.
- * @return {Promise<[{}]>}  A promise to the properties.
- */
 const getAllProperties = function (options, limit = 10) {
   const queryParams = [];
   const len = queryParams.length;
@@ -109,12 +81,12 @@ const getAllProperties = function (options, limit = 10) {
 
   if (options.owner_id) {
     queryParams.push(options.owner_id);
-    queryString += `WHERE properties.owner_id = $${queryParams.length} `;
+    queryString += `WHERE properties.owner_id = $${len} `;
   }
 
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `AND city ILIKE $${queryParams.length} `;
+    queryString += `AND city ILIKE $${len} `;
   }
 
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
@@ -139,7 +111,7 @@ const getAllProperties = function (options, limit = 10) {
   queryString += `
   GROUP BY properties.id
   ORDER BY cost_per_night
-  LIMIT $${queryParams.length};
+  LIMIT $${len};
   `;
 
   console.log(queryString, queryParams);
@@ -148,15 +120,31 @@ const getAllProperties = function (options, limit = 10) {
 };
 exports.getAllProperties = getAllProperties;
 
-/**
- * Add a property to the database
- * @param {{}} property An object containing all of the property details.
- * @return {Promise<{}>} A promise to the property.
- */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+  INSERT INTO properties (
+    owner_id,
+    title,
+    description, 
+    number_of_bedrooms,
+    number_of_bathrooms,
+    parking_spaces,
+    cost_per_night,
+    thumbnail_photo_url,
+    cover_photo_url,
+    street,
+    country,
+    city, 
+    province,
+    post_code
+    ) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
+  RETURNING *;
+  `;
+
+  return db
+    .query(queryString, Object.values(property))
+    .then((res) => res.rows[0])
+    .catch((err) => console.error(err.message));
 };
 exports.addProperty = addProperty;
